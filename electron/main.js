363aabe73b43c687f91e2351f73b22111c9ac5e3
have2go <hypercrock@yandex.ru>
2022-12-27 15:33:39 +0300

    Создал и настроил приложение React + Electron

---
+++ electron/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require('electron-is-dev');
+
+const path = require('path');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        // webPreferences: {
+        //     preload: path.join(__dirname, 'preload.js')
+        // }
+    });
+    // win.loadFile("index.html");
+    win.loadURL(
+        isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`
+    );
+    if (isDev) {
+        win.webContents.openDevTools();
+    }
+};
+
+// окно может быть создано только когда произодйет событие ready для модуля app
+app.whenReady().then(() => {
+    createWindow();
+
+    //это для MacOS, тк при закрытии окна приложение продолжает работать
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
