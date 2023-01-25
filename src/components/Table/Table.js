7cf2f572135f282289e78ebd8921709d18139741
have2go <hypercrock@yandex.ru>
2023-01-25 12:43:15 +0300

    Добавил поиск по колонкам

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -3,13 +3,14 @@ import "./Table.css";
 import BootstrapTable from "react-bootstrap-table-next";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import filterFactory from 'react-bootstrap-table2-filter';
 
 import {columns} from '../utils/constants';
 
 
 
 function Table({books}) {
-    return <BootstrapTable classes="table" condensed bootstrap4 striped hover keyField="id" data={books} columns={columns} />;
+    return <BootstrapTable classes="table" condensed bootstrap4 striped hover keyField="id" data={books} columns={columns} filter={filterFactory()} />;
 }
 
 export default Table;
--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,4 +1,6 @@
- export let columns = [
+import { textFilter } from 'react-bootstrap-table2-filter';
+ 
+ export const columns = [
     {
         dataField: "id",
         text: "№",
@@ -6,30 +8,37 @@
     {
         dataField: "date",
         text: "Дата постановки",
+        filter: textFilter()
     },
     {
         dataField: "department",
         text: "Кафедра",
+        filter: textFilter()
     },
     {
         dataField: "title",
         text: "Название",
+        filter: textFilter()
     },
     {
         dataField: "vi",
         text: "ВИ",
+        filter: textFilter()
     },
     {
         dataField: "sp",
         text: "СП",
+        filter: textFilter()
     },
     {
         dataField: "editor",
         text: "Редактор",
+        filter: textFilter()
     },
     {
         dataField: "layout",
         text: "Верстальщик",
+        filter: textFilter()
     },
     {
         dataField: "pages",
@@ -49,47 +58,47 @@
     },
 ];
 
-export let books = [
-    {
-        id: 1,
-        date: "00.00.0000",
-        department: "Е1",
-        title: "Книга",
-        vi: "У",
-        sp: "Т",
-        editor: "З",
-        layout: "СВ",
-        pages: "45",
-        volume: "5500",
-        printing: "100",
-        year: "2005",
-    },
-    {
-        id: 2,
-        date: "00.00.0000",
-        department: "Е1",
-        title: "Книга",
-        vi: "У",
-        sp: "Т",
-        editor: "З",
-        layout: "СВ",
-        pages: "45",
-        volume: "5500",
-        printing: "100",
-        year: "2005",
-    },
-    {
-        id: 3,
-        date: "00.00.0000",
-        department: "Е1",
-        title: "Книга",
-        vi: "У",
-        sp: "Т",
-        editor: "З",
-        layout: "СВ",
-        pages: "45",
-        volume: "5500",
-        printing: "100",
-        year: "2005",
-    },
-];
+// export let books = [
+//     {
+//         id: 1,
+//         date: "00.00.0000",
+//         department: "Е1",
+//         title: "Книга",
+//         vi: "У",
+//         sp: "Т",
+//         editor: "З",
+//         layout: "СВ",
+//         pages: "45",
+//         volume: "5500",
+//         printing: "100",
+//         year: "2005",
+//     },
+//     {
+//         id: 2,
+//         date: "00.00.0000",
+//         department: "Е1",
+//         title: "Книга",
+//         vi: "У",
+//         sp: "Т",
+//         editor: "З",
+//         layout: "СВ",
+//         pages: "45",
+//         volume: "5500",
+//         printing: "100",
+//         year: "2005",
+//     },
+//     {
+//         id: 3,
+//         date: "00.00.0000",
+//         department: "Е1",
+//         title: "Книга",
+//         vi: "У",
+//         sp: "Т",
+//         editor: "З",
+//         layout: "СВ",
+//         pages: "45",
+//         volume: "5500",
+//         printing: "100",
+//         year: "2005",
+//     },
+// ];
